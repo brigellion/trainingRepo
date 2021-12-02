fbd2fdaf647137a94080f67b3fa584903d9c16a0
Andrey Poymanov <poym4nov@yandex.ru>
2021-12-02 23:01:02 +0300

    develop

--- script.js
+++ script.js
@@ -2,30 +2,39 @@
 
 let magicNumber = 77;
 
-function queryNumber(num) {
-    function question() {
-        const input = prompt('Угадай число от 1 до 100');
-        if (input) {
-            if (typeof input == 'number') {
+function game(amountTry, num) {
 
-                if (input > num) {
-                    alert('Загаданное число меньше');
+    function queryNumber(num) {
+        function question() {
+            const input = prompt('Угадай число от 1 до 100');
+            if (input) {
+                if (typeof input == 'number') {
+                    if (amountTry !== 0) {
+                        amountTry--;
+                    } else {
+                        return;
+                    }
+                    if (input > num) {
+                        alert('Загаданное число меньше, осталось попыток ' + amountTry);
+                        question();
+                    } else if (input < num) {
+                        alert('Загаданное число больше');
+                        question();
+                    } else if (input === num) {
+                        alert('Поздравляю, Вы угадали!!!');
+                    }
+                } else {
+                    alert('Введи число!');
                     question();
-                } else if (input < num) {
-                    alert('Загаданное число больше');
-                    question();
-                } else if (input === num) {
-                    alert('Поздравляю, Вы угадали!!!');
                 }
             } else {
-                alert('Введи число!');
-                question();
+                alert('Игра окончена');
             }
-        } else {
-            alert('Игра окончена');
         }
+        question();
     }
-    question();
+    queryNumber(num);
 }
 
-queryNumber(magicNumber);
+//queryNumber(magicNumber);
+game(10, magicNumber);
