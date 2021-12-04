d5436087d05bcbcef480598965d8b8a055bb2234
Andrey Poymanov <poym4nov@yandex.ru>
2021-12-04 10:58:06 +0300

    fix argument

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 'use strict';
 
-let magicNumber = 66;
+function getRandomNumber() {
+    return Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+}
 
-function game(num, amountTry) {
-    function queryNumber(num) {
-        function question() {
-            let input = prompt('Угадай число от 1 до 100');
-            if (input === null) {
-                console.log(input, typeof input);
-                alert('Игра окончена');
-            } else {
-                input = Number(input);
-                if (input > 0) {
-                    if (--amountTry) {
-                        if (input > num) {
-                            alert('Загаданное число меньше, осталось попыток ' + amountTry);
-                            question();
-                        } else if (input < num) {
-                            alert('Загаданное число больше, осталось попыток ' + amountTry);
-                            question();
-                        } else if (input === num) {
-                            if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
-                                game(Math.floor(Math.random() * (10 - 1 + 1)) + 1, 10);
-                            }
-                        }
-                    } else {
-                        if (confirm('Попытки закончились, хотите сыграть еще?')) {
-                            game(magicNumber, 10);
+function queryNumber() {
+    let magicNumber = getRandomNumber(),
+        amountTry = 10;
+    function question() {
+        let input = prompt('Угадай число от 1 до 100');
+        if (input === null) {
+            alert('Игра окончена');
+        } else {
+            input = Number(input);
+            if (input > 0) {
+                if (--amountTry) {
+                    if (input > magicNumber) {
+                        alert('Загаданное число меньше, осталось попыток: ' + amountTry);
+                        question();
+                    } else if (input < magicNumber) {
+                        alert('Загаданное число больше, осталось попыток: ' + amountTry);
+                        question();
+                    } else if (input === magicNumber) {
+                        if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
+                            queryNumber();
                         }
                     }
                 } else {
-                    alert('Введи число!');
-                    question();
+                    if (confirm('Попытки закончились, хотите сыграть еще?')) {
+                        queryNumber();
+                    }
                 }
+            } else {
+                alert('Введи число!');
+                question();
             }
         }
-        question();
     }
-    queryNumber(num);
+    question();
 }
 
-game(magicNumber, 10);
+queryNumber();
